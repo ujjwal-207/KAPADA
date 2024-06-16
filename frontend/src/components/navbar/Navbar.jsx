d8477b09043936f5662460508fc1e3877f57cd60
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2024-06-16 09:42:38 +0545

    inital

---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './navbar.css';
+import logo from "../Assets/logo.png"
+import cart_icon from "../Assets/cart_icon.png"
+export class Navbar extends Component {
+  render() {
+    return (
+      <div className='navbar'>
+        
+        
+      </div>
+    )
+  }
+}
+
+export default Navbar
+
