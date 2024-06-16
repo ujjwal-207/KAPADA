355b007af2d73485cf0104f0d8607ad1d3a1f7b7
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2024-06-16 22:23:29 +0545

    popular section done

--- frontend/src/components/popular/popular.jsx
+++ frontend/src/components/popular/popular.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import './popular.css'
+import data_product from "../Assets/data"
+import Item from '../item/item'
 const Popular = () => {
   return (
     <div className='popular'>
-      
+      <h1>
+        Popular In Women
+        <hr />
+      </h1>
+      <div className='popular-item'>
+        {data_product.map((item,i) =>{
+          return(<Item key={i} 
+          id={item.id} 
+          name={item.name} 
+          image={item.image} 
+          new_price={item.new_price} 
+          old_price={item.old_price} /> )
+        })}
+      </div>
     </div>
   )
 }
--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Hero from '../components/hero/Hero'
+import Popular from '../components/popular/popular'
+import Offers from '../components/offers/offers'
+import Newcollection from '../components/newcollection/newcollection'
 
 const Shop = () => {
   return (
     <div>
       <Hero/>
+      <Popular />
+      <Offers />
+      <Newcollection />
     </div>
   )
 }
