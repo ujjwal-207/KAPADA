c3c6374c9035a6d1eafd9b4084a36ca475022037
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2024-06-17 22:56:31 +0545

    product pages

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/shopcontext'
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../components/breadcrum.css/breadcrum';
+import ProductDisplay from '../components/productDisplay/productDisplay';
 
 export default function Product() {
+  const {all_product} =useContext(ShopContext);
+  const {productId} =useParams();
+  const product = all_product.find((e)=>e.id===Number(productId));
+
   return (
     <div>
-      
+        <Breadcrum product={product} />
+        <ProductDisplay product={product}/>
     </div>
   )
 }
