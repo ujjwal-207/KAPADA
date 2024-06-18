acd9306723a5d3ba5238913f10e96f3826223a1c
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2024-06-18 20:20:03 +0545

    responsive navbar

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,23 +1,29 @@
-import React, { Component, useContext, useState } from 'react'
+import React, { Component, useContext, useRef, useState } from 'react'
 import './navbar.css';
 import logo from "../Assets/logo.png"
 import cart_icon from "../Assets/cart_icon.png"
+import nav_dropdown from '../Assets/dropdown.jpeg'
 import { Link } from 'react-router-dom';
 import { useCallback } from 'react';
 import { ShopContext } from '../../context/shopcontext';
 function Navbar(){
   const[menu,setMenu] =useState("shop");
-  const{getTotalCartItems} =useContext(ShopContext)
-
+  const{getTotalCartItems} =useContext(ShopContext);
+  const menuRef =useRef();
+    const dropdown_toggle =(e)=>{
+      menuRef.current.classlist.toggle('nav-menu-visible');
+      e.target.classlist.toggle('open');
+    }
 
 
     return (
       <div className='navbar'>
         <div className='nav-logo'>
           <img src={logo} alt='logo' />
-          <p onClick={() =>{setMenu("shop")}}><Link style={{textDecoration:"none"}} to='/'>Kapada</Link>{menu ==="shop"?<hr />:<></>}</p>
+          <p onClick={() =>{setMenu("shop")}}><Link style={{textDecoration:"none"}} to='/'>Kapada</Link></p>
         </div>
-        <ul className='nav-menu'>
+        <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt=''/>
+        <ul ref={menuRef} className='nav-menu'>
           <li onClick={() =>{setMenu("shop")}}><Link style={{textDecoration:"none"}} to='/'>Shop</Link>{menu ==="shop"?<hr />:<></>}</li>
           <li onClick={() =>{setMenu("mens")}}><Link style={{textDecoration:"none"}} to='/mens'>Men</Link>{menu ==="mens"?<hr />:<></>}</li>
           <li onClick={() =>{setMenu("womens")}}><Link style={{textDecoration:"none"}} to='/womens'>Women</Link>{menu ==="womens"?<hr />:<></>}</li>
