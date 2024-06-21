d614c545cca1029c8a205fb9fcb36491991957aa
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2024-06-21 13:15:09 +0545

    addproduct frontend

--- admin/src/Pages/Admin/admin.jsx
+++ admin/src/Pages/Admin/admin.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './admin.css'
 import Sidebar from '../../components/sidebar/sidebar'
+import {Routes,Route} from 'react-router-dom'
+import Addproduct from '../../components/addproduct/addproduct'
+import ListProduct from '../../components/listproduct/listproduct'
 const Admin = () => {
   return (
     <div className='admin'>
       <Sidebar />
+      <Routes>
+        <Route path='/addproduct' element={<Addproduct />}></Route>
+        <Route path='/listproduct' element={<ListProduct />}></Route>
+      </Routes>
+      
     </div>
   )
 }
---
+++ admin/src/components/listproduct/listproduct.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './listproduct.css'
+const ListProduct = () => {
+  return (
+    <div className='list-product'>
+      
+    </div>
+  )
+}
+
+export default ListProduct
