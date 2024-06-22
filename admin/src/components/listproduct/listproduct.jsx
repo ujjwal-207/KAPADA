2ac463333a0f8bcb386ebabd6f0fc2b0a947969d
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2024-06-22 22:11:10 +0545

    repuees sign

--- admin/src/components/listproduct/listproduct.jsx
+++ admin/src/components/listproduct/listproduct.jsx
@@ -47,8 +47,8 @@ const ListProduct = () => {
             <div key ={index} className="listproduct-format-main listproduct-format">
               <img src={product.image} alt="" className="listproduct-product-icon" />
               <p>{product.name}</p>
-              <p>{product.old_price}</p>
-              <p>{product.new_price}</p>
+              <p>रु{product.old_price}</p>
+              <p>रु{product.new_price}</p>
               <p>{product.category}</p>
               <img onClick={()=>{remove_product(product.id)}}src={cross_icon} alt="" className="listproduct-remove-icon" />
             </div>
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
-const port =5000;
+
 const express = require("express");
-const app = express();
+
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
-const { error, log } = require("console");
-const { type } = require("os");
+
+//app config
+const app = express();
+const port =5000;
 
 app.use(express.json());
 app.use(cors());
