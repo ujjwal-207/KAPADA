180c7f1efb3ce31d0126dcf80e56788e0946b097
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2024-06-24 19:34:26 +0545

    backend

--- frontend/public/constant.js
+++ frontend/public/constant.js
@@ -1,7 +1,7 @@
 //So that I can fetch data in both development and deployment
 
 const production = {
-    url: 'https://kapada-frontend.onrender.com'
+    url: 'https://kapada-backend.onrender.com'
     
   };
   const development = {
