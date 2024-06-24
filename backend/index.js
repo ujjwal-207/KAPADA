9a01e344fe243b97b53310f8ba60147ada0e1765
Ujjwal Nepal <ujjwalnepal2060@gmail.com>
2024-06-24 20:02:07 +0545

    image url dynamically

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,7 @@ const port = process.env.PORT ||5000;
 
 app.use(express.json());
 app.use(cors());
-import { config } from "../admin/public/constant";
-const URL =config.url;
+
 
 //Database Connection With  MongoDB
 
@@ -40,10 +39,11 @@ const upload=multer({storage:storage});
 app.use('/images',express.static('upload/images'))
 
 app.post("/upload",upload.single('product'),(req,res)=>{
-    
+    const protocol = req.protocol;
+    const host = req.get('host');
     res.json({
         sucess:1,
-        image_url:`${URL}/images/${req.file.filename}`
+        image_url:`${protocol}://${host}/images/${req.file.filename}`
     })
 });
 //Schema for Creating Products
